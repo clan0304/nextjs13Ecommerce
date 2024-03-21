304ee81af8c63bf9666bb8c40b7e019238c4726c
clan0304 <clan0304@gmail.com>
2024-03-21 21:49:24 +1100

    changed

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,25 +1,16 @@
 import prisma from '@/app/libs/prismadb';
 import { NextResponse } from 'next/server';
 
-export default async function (
+export async function GET(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const { method } = req;
-
-  if (method === 'GET') {
-    const { id } = params;
-    const product = await prisma.product.findUnique({
-      where: {
-        id: id,
-      },
-    });
-
-    return NextResponse.json(product);
-  }
-
-  return new Response(null, {
-    status: 405,
-    statusText: 'Method Not Allowed',
+  const { id } = params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: id,
+    },
   });
+
+  return NextResponse.json(product);
 }
