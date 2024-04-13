38a686a86bb5e5caa78ae9baab74c056fafbac04
clan0304 <clan0304@gmail.com>
2024-04-13 21:57:17 +1000

    chagned

--- components/modals/ProfileModal.tsx
+++ components/modals/ProfileModal.tsx
@@ -9,10 +9,13 @@ const ProfileModal = () => {
     <div>
       {profileMenuModal.isOpen && (
         <div className="bg-white text-black flex flex-col w-full border-2 border-black rounded-lg">
-          <div>
-            <p className="ml-2 text-sm md:text-md">Order</p>
+          <div className="hover:bg-indigo-200">
+            <p className="ml-2 text-sm md:text-md ">Order</p>
           </div>
-          <div className="border-black border-t-2" onClick={() => signOut}>
+          <div
+            className="border-black border-t-2 hover:bg-indigo-200"
+            onClick={() => signOut}
+          >
             <p className="ml-2 text-sm md:text-md">Sign Out</p>
           </div>
         </div>
