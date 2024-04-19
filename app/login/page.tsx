d17ffe3fcf4b07489935d6cc7827ff0cbe0a4dfb
clan0304 <clan0304@gmail.com>
2024-04-19 21:16:28 +1000

    changed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
         <div role="status">
           <svg
             aria-hidden="true"
-            className="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-indigo-600"
+            className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-indigo-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
