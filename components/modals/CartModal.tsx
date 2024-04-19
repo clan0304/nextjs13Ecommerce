ad1f0f04bf6b02fe3f6eb2651b2326ab0dfbeab5
clan0304 <clan0304@gmail.com>
2024-04-19 19:31:12 +1000

    changed

--- components/modals/CartModal.tsx
+++ components/modals/CartModal.tsx
@@ -31,7 +31,7 @@ const CartModal = () => {
   const handleCheckout = async () => {
     try {
       setIsLoading(true);
-      const res = await fetch('http://localhost:3000/api/orders', {
+      const res = await fetch('/api/orders', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
